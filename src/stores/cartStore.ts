04764439ce9f74696991059741104e60cfdae4cc
leobewater <mmbee888@gmail.com>
2023-09-28 15:42:18 -0400

    set cart as item type

--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -1,8 +1,9 @@
 // cart store
-import { atom } from 'recoil';
 // An atom represents a piece of state. Atoms can be read from and written to from any component.
+import { atom } from 'recoil';
+import { Item } from '../models/Item';
 
-export const cartState = atom({
+export const cartState = atom<Item[]>({
   key: 'cart',
-  default: 0,
+  default: [],
 });
